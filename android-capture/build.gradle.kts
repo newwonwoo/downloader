f0af5f7e57cd4b97f78317e52c7b6e7a39ea1d2c
newwonwoo <maspiece83@gmail.com>
2026-09-16 01:07:38 +0900

    chore(android): bump direct-download app version

--- android-capture/build.gradle.kts
+++ android-capture/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.newwonwoo.downloader.capture"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
