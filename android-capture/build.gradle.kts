1040b879937a13356fd6954aaa67ca1b4c9f0043
newwonwoo <maspiece83@gmail.com>
2026-09-16 16:08:57 +0900

    chore(android): bump direct capture to v1.3

--- android-capture/build.gradle.kts
+++ android-capture/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.newwonwoo.downloader.capture"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
